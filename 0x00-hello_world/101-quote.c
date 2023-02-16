0cdf5b635ded63899bdbe9446095f0cc7e40b1a9
Oluwaseun-93 <oluwanifiseoluwaseunhannah@gmail.com>
2023-02-16 14:33:07 -0800

    101 quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,12 +5,12 @@
 /**
  * main - entry
  *
- * Return: 1 on success
+ * Return: 0 on success
  */
 
 int main(void)
 
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n,stdout, 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout, 59);
 	return (1);
 }
