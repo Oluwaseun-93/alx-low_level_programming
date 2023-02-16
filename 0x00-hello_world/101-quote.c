c2b2df09d02a2a9e1271336e58f3f43853fc812b
Oluwaseun-93 <oluwanifiseoluwaseunhannah@gmail.com>
2023-02-16 13:49:58 -0800

    size

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 #include <unistd.h>
+
+
 /**
  * main - entry
  *
- * Return: 1
+ * Return: 1 on success
  */
+
 int main(void)
+
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar\n, 2015-10-19, 59);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n, stdout");
 	return (1);
 }
