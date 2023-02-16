6517ed3cdf1f074a14efb5d61c47847e31bf41b4
Oluwaseun-93 <oluwanifiseoluwaseunhannah@gmail.com>
2023-02-16 13:04:37 -0800

    101 quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - entry
+ *
+ * Return: 1
+ */
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar\n, 2015-10-19, 59);
+	return (1);
+}
