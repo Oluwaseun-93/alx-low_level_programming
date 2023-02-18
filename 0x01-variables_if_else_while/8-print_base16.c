4b2834a3a88d861f4d0f6d03347e4776d9584614
Oluwaseun-93 <oluwanifiseoluwaseunhannah@gmail.com>
2023-02-17 21:10:36 -0800

    base 16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,27 +1,20 @@
 #include <stdio.h>
-#include <unistd.h>
 
 /**
  * main - main function
- * Description: Print base 16 numbers
+ *
  * Return: always 0
  */
 
 int main(void)
 {
-	char c;
-	char d = '0';
+	int i;
+	int j;
 
-	while
-		(d <= '9');
-	{
-		putchar(d);
-		d++;
-	}
-	for (c = 'a' ; c <= 'f' ; c++)
-	{
-		putchar(c);
-	}
+	for (i = 0 ; i < 10 ; i++)
+		putchar(i);
+	for (j = 'a' ; j <= 'f' ; j++)
+		putchar(j);
 	putchar('\n');
 	return (0);
 }
